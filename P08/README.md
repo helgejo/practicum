e61b06bd269d00ab033366bcb13d175f1af92b92
Krisztian Balog <krisztian.balog@uis.no>
2015-10-16 10:10:26 +0200

    P08, Task 1

---
+++ P08/README.md
@@ -0,0 +1,55 @@
+Practicum 8
+===========
+
+Learning objectives:
+
+  - Indexing and searching a document collection using Whoosh
+  - Evaluating retrieval using various search effectiveness metrics
+
+
+## Task 0. Installing Whoosh
+
+  - We will be using the [Whoosh](https://pypi.python.org/pypi/Whoosh/) full-text indexing and searching Python library.
+  - You can download and install it using `easy_install Whoosh` or `pip install Whoosh`.
+
+
+## Task 1. Indexing documents with Whoosh
+
+  - Build an index of a toy document collection using Whoosh.
+    * Add `title` and `content` as indexed
+    * See [this page](http://whoosh.readthedocs.org/en/latest/schema.html) for details on how to design the index schema
+  - Load the index and check whether everything was done correctly by requesting the following information from it:
+    * Number of documents the index contains
+    * Number of times the term `document` appears in the title and content fields (i.e., term frequency)
+    * Number of documents the term `document` appears in the title and content fields (i.e., document frequency)
+    * Total length (number of terms) in the title and content fields
+    * The posting list for the term `document` for the content field
+  - See the [IndexReader](http://whoosh.readthedocs.org/en/latest/api/reading.html) and the [Whoosh recipes](http://whoosh.readthedocs.org/en/latest/recipes.html) pages for help.
+  
+
+## Task 2. Indexing the CACM collection with Whoosh
+
+  - The CACM collection in `data/cacm.xml` contains 3024 documents.
+  - Build an index of this collection using Whoosh by selecting the appropriate [Analyzer](http://whoosh.readthedocs.org/en/latest/api/analysis.html).
+  - Check index statistics (see Task 1 for ideas).
+
+
+## Task 3. Implement TFIDF retrieval by getting statistics from the Woosh index
+
+
+
+## Task 4. Ranking documents using Whoosh
+
+
+
+## Task 5. Evaluating results
+
+  - The ground truth is in `data/cacm.rel`. The format is `queryID Q0 docID rel` where `rel` is 1 if the document is relevant and 0 otherwise. 
+    * This file contains only the relevant documents so the value will always be 1. This means that everything that is not in this file counts as non-relevant.
+  - Write a script that computes P@5, P@10, Average Precision, and Reciprocal Ranks for each query as well as the averages over the entire query set.
+
+
+## References
+
+  - [Whoosh documentation](http://whoosh.readthedocs.org/en/latest/)
+  - [Whoosh recipes](http://whoosh.readthedocs.org/en/latest/recipes.html)
---
+++ P08/tasks/task1.py
@@ -0,0 +1,88 @@
+# Indexing documents with Whoosh
+# ==============================
+
+# Task
+# ------
+#   - Build an index of a toy document collection using Whoosh.
+#     * Add `title` and `content` as indexed
+#     * See [this page](http://whoosh.readthedocs.org/en/latest/schema.html) for details on how to design the index schema
+#   - Load the index and check whether everything was done correctly by requesting the following information from it:
+#     * Number of documents the index contains
+#     * Number of times the term `document` appears in the title and content fields (i.e., term frequency)
+#     * Number of documents the term `document` appears in the title and content fields (i.e., document frequency)
+#     * Total length (number of terms) in the title and content fields
+#     * The posting list for the term `document` for the content field
+#   - See the [IndexReader](http://whoosh.readthedocs.org/en/latest/api/reading.html)
+#     and the [Whoosh recipes](http://whoosh.readthedocs.org/en/latest/recipes.html) pages for help.
+
+# Solution
+# --------
+
+import os.path
+import whoosh.index as index
+from whoosh.fields import Schema, ID, TEXT
+
+# Document collection
+docs = [
+    {'id': "001", 'title': "My document", 'content': "This is my document!"},
+    {'id': "002", 'title': "Second try", 'content': "This is my second example."},
+    {'id': "003", 'title': "Third time's the charm", 'content': "Examples are many."},
+    {'id': "004", 'title': "Final example document", 'content': "Document, second document, third document, my document."},
+]
+
+index_dir = "index"
+
+
+def create_index():
+    # TODO We create the index schema.
+    schema = Schema()
+
+    if not os.path.exists(index_dir):
+        os.mkdir(index_dir)
+    ix = index.create_in(index_dir, schema)
+
+    # The writer() method of the Index object returns an IndexWriter object that lets us add documents to the index.
+    writer = ix.writer()
+
+    # Add documents to the index.
+    for doc in docs:
+        # TODO add document
+        # Notes:
+        # - Indexed text fields must be passed a unicode value. (use "str".decode())
+        # - Fields can be left empty, i.e., we don't have to fill in a value for every field.
+        writer.add_document()
+
+    # Calling commit() on the IndexWriter saves the added documents to the index.
+    writer.commit()
+
+    ix.close()
+
+
+def read_index():
+    # Check if index exists in `index_dir`
+    if not index.exists_in(index_dir):
+        print "Index does not exist"
+        return None
+
+    # Open index
+    ix = index.open_dir(index_dir)
+
+    # Use the reader to get statistics
+    reader = ix.reader()
+
+    # TODO Number of documents the index contains
+
+    # TODO Number of times the term `document` appears in the title and content fields (i.e., term frequency)
+
+    # TODO Number of documents the term `document` appears in the title and content fields (i.e., document frequency)
+
+    # TODO Total length (number of terms) in the title and content fields
+
+    # TODO The posting list for the term `document` for the content field
+
+    ix.close()
+
+
+if __name__ == '__main__':
+    create_index()
+    #read_index()
