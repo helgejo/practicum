0e0a48566b5c0dace0afe6e07a6c059f42d6be7c
Krisztian Balog <krisztian.balog@uis.no>
2015-10-17 22:05:19 +0800

    P08, Task 5

--- P08/solutions/task5.py
+++ P08/solutions/task5.py
@@ -16,6 +16,115 @@
 
 from __future__ import division
 
-import math
+gt_file = "../data/cacm.rel"  # ground truth file with relevance judgments
+res_file = "../data/cacm.out"  # retrieval results file, i.e., output of batch retrieval
 
 
+# Load the ground truth file.
+# It returns a dictionary with queryID as key and a list of relevant documents as value.
+def load_gt(gt_file):
+    gt = {}
+    for line in open(gt_file, "r"):
+        tmp = line.split(" ")
+        query_id = tmp[0]
+        doc_id = tmp[2]
+        rel = tmp[3]
+        if int(rel) == 1:  # we only consider relevant documents
+            if query_id not in gt:
+                gt[query_id] = []
+            gt[query_id].append(doc_id)
+    return gt
+
+
+# Load the retrieval results file.
+# It returns a dictionary with queryID as key and a list of retrieved documents as value.
+# We assume that the document in the file are ordered from more relevant the less relevant.
+def load_res(run_file):
+    res = {}
+    for line in open(run_file, "r"):
+        tmp = line.split(" ")
+        query_id = tmp[0]
+        doc_id = tmp[2]
+        # Note: we do not use the actual retrieval score, but we do assume that
+        # the documents are ordered by descreasing order of relevance
+        if query_id not in res:
+            res[query_id] = []
+        res[query_id].append(doc_id)
+    return res
+
+
+# Computes various evaluation metrics for a given query
+def evaluate_query(gt, res):
+    eval = {
+        'P5': 0.0,
+        'P10': 0.0,
+        'RR': 0.0,
+        'AP': 0.0
+    }
+
+    # We iterate through the returned results
+    rel = 0  # relevant documents returned up till now
+    for idx, doc_id in enumerate(res):
+        rank = idx + 1  # this is just for our own convenience
+        if doc_id in gt:  # relevant document retrieved
+            rel += 1
+            # mind that there might be less than 5 (10) results returned,
+            # so we update P5 (P10) continuously
+            if rank <= 5:
+                eval['P5'] = rel / 5.0
+            if rank <= 10:
+                eval['P10'] = rel / 10.0
+            # If this is the first relevant document encounter
+            if rel == 1:
+                eval['RR'] = 1.0 / rank
+            # For AP we count precision after each relevant retrieved document
+            eval['AP'] += rel / rank
+
+    # We need to normalize AP by the total number of relevant documents (in the ground truth)
+    eval['AP'] /= len(gt)
+
+    return eval
+
+
+# Evaluate a results file against a ground truth file.
+def evaluate(gt_file, res_file):
+    gt = load_gt(gt_file)
+    res = load_res(res_file)
+
+    eval = {}  # stores evaluation scores for each query and metric
+    eval_avg = {}  # stores averaged evaluation scores for each metric
+
+    # Evaluate each query in the ground truth
+    for query_id in sorted(gt):
+        # Note that there might not be results in the results file for the given query;
+        # in such cases we take the results be an empty list (and we need to make sure we can
+        # handle empty lists in `evaluate_query`).
+        eval_q = evaluate_query(gt[query_id], res[query_id] if query_id in res else [])
+        # Save the results
+        eval[query_id] = eval_q
+        for metric in eval_q:
+            if metric not in eval_avg:
+                eval_avg[metric] = 0
+            eval_avg[metric] += eval_q[metric]
+
+    # Compute averages
+    for metric in eval_avg:
+        eval_avg[metric] /= len(gt)
+
+    # Print the results
+    metrics = ["P5", "P10", "AP", "RR"]
+    line = "-" * (10 + 9 * len(metrics))
+    print "{:<10}".format("query_id"), ("{:>8}" * len(metrics)).format(*metrics)
+    print line
+    # Per-query results
+    for query_id in sorted(eval):
+        scores = [str(eval[query_id][metric])[:5] for metric in ["P5", "P10", "AP", "RR"]]
+        print "{:<10}".format(query_id), ("{:>8}" * len(metrics)).format(*scores)
+    # Average results
+    print line
+    scores = [str(eval_avg[metric])[:5] for metric in ["P5", "P10", "AP", "RR"]]
+    print "{:<10}".format("Average"), ("{:>8}" * len(metrics)).format(*scores)
+
+
+if __name__ == "__main__":
+    evaluate(gt_file, res_file)
--- P08/tasks/task5.py
+++ P08/tasks/task5.py
@@ -18,4 +18,96 @@ from __future__ import division
 
 import math
 
+gt_file = "../data/cacm.rel"  # ground truth file with relevance judgments
+res_file = "../data/cacm.out"  # retrieval results file, i.e., output of batch retrieval
 
+
+# Load the ground truth file.
+# It returns a dictionary with queryID as key and a list of relevant documents as value.
+def load_gt(gt_file):
+    gt = {}
+    for line in open(gt_file, "r"):
+        tmp = line.split(" ")
+        query_id = tmp[0]
+        doc_id = tmp[2]
+        rel = tmp[3]
+        if int(rel) == 1:  # we only consider relevant documents
+            if query_id not in gt:
+                gt[query_id] = []
+            gt[query_id].append(doc_id)
+    return gt
+
+
+# Load the retrieval results file.
+# It returns a dictionary with queryID as key and a list of retrieved documents as value.
+# We assume that the document in the file are ordered from more relevant the less relevant.
+def load_res(run_file):
+    res = {}
+    for line in open(gt_file, "r"):
+        tmp = line.split(" ")
+        query_id = tmp[0]
+        doc_id = tmp[2]
+        # Note: we do not use the actual retrieval score, but we do assume that
+        # the documents are ordered by descreasing order of relevance
+        if query_id not in res:
+            res[query_id] = []
+        res[query_id].append(doc_id)
+    return res
+
+
+# Computes various evaluation metrics for a given query
+def evaluate_query(gt, res):
+    eval = {
+        'P5': 0.0,
+        'P10': 0.0,
+        'RR': 0.0,
+        'AP': 0.0
+    }
+
+    # TODO compute P5, P10, RR, and AP for the given query
+
+    return eval
+
+
+# Evaluate a results file against a ground truth file.
+def evaluate(gt_file, res_file):
+    gt = load_gt(gt_file)
+    res = load_res(res_file)
+
+    eval = {}  # stores evaluation scores for each query and metric
+    eval_avg = {}  # stores averaged evaluation scores for each metric
+
+    # Evaluate each query in the ground truth
+    for query_id in sorted(gt):
+        # Note that there might not be results in the results file for the given query;
+        # in such cases we take the results be an empty list (and we need to make sure we can
+        # handle empty lists in `evaluate_query`).
+        eval_q = evaluate_query(gt[query_id], [] if query_id not in res else res[query_id])
+        # Save the results
+        eval[query_id] = eval_q
+        for metric in eval_q:
+            if metric not in eval_avg:
+                eval_avg[metric] = 0
+            eval_avg[metric] += eval_q[metric]
+
+    # Compute averages
+    for metric in eval_avg:
+        eval_avg[metric] /= len(gt)
+
+    # Print the results
+    metrics = ["P5", "P10", "AP", "RR"]
+    line = "-" * (10 + 9 * len(metrics))
+    print "{:<10}".format("query_id"), ("{:>8}" * len(metrics)).format(*metrics)
+    print line
+    # Per-query results
+    for query_id in sorted(eval):
+        scores = [str(eval[query_id][metric])[:5] for metric in ["P5", "P10", "AP", "RR"]]
+        print "{:<10}".format(query_id), ("{:>8}" * len(metrics)).format(*scores)
+    # Average results
+    print line
+    scores = [str(eval_avg[metric])[:5] for metric in ["P5", "P10", "AP", "RR"]]
+    print "{:<10}".format("Average"), ("{:>8}" * len(metrics)).format(*scores)
+
+
+if __name__ == "__main__":
+    evaluate(gt_file, res_file)
